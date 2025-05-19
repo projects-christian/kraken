72b12afabcec42b72e40fb25f366e88b60ce96f6
Christian.cervantes <christian.cervantes.d@gmail.com>
2025-05-19 16:20:08 -0500

    adding hero images, favicon and logo

--- src/components/LogoBox.vue
+++ src/components/LogoBox.vue
@@ -1,9 +1,9 @@
 <template>
-  <router-link :to="{name:'landing'}" class="logo">
-    <img :src="logo" class="h-10" alt="WebAi Logo">
-  </router-link>
+    <router-link :to="{ name: 'landing' }" class="logo">
+        <img :src="logo" class="h-10" alt="WebAi Logo" />
+    </router-link>
 </template>
 
 <script setup lang="ts">
-import logo from "@/assets/images/kraken-logo.png";
+import logo from "@/assets/images/logo-kraken.png";
 </script>
