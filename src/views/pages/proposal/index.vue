a1694464556afb6131ddaa9c1d46d6df3c8cf40b
christian <christian.cervantes.d@gmail.com>
2025-11-05 22:22:40 -0500

    feat: change name to carousel

--- src/views/pages/proposal/index.vue
+++ src/views/pages/proposal/index.vue
@@ -7,5 +7,5 @@
 <script setup lang="ts">
     import './proposal.scss'
     import Header from "@/views/pages/proposal/components/Header.vue";
-    import Carousel from "@/views/pages/proposal/components/Carousel.vue";
+    import Carousel from "@/views/pages/proposal/components/CarouselProposal.vue";
 </script>
